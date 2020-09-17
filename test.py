f2ea5f8a889519cd4f4a7c4623431757d1540187
张云放 <62100077+Xinwuyaqu@users.noreply.github.com>
2020-09-17 09:11:26 +0800

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-directory = os.walk('E:\\')
-lists = open('D:\\Music\\list.txt', 'w')
-for dirs in directory:
-    for file in dirs[2]:
-        if '.mp3' in file:
-            lists.writelines(file+'\n')
-            print(file)
